527dcd8cb637144fd290b48c5993b21eadf66d01
Bartlomiej Mosurek <bartlomiej.mosurek@gmail.com>
2021-07-31 13:51:03 +1000

    Add: script.js Exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,8 @@
     templateOf: {
       bookTemplate: '#template-book'
     },
-    bookList: '.books-list'
+    bookList: '.books-list',
+    bookImages:'.book__image'
 
 
   };
@@ -13,15 +14,31 @@
     bookTemplate: Handlebars.compile(document.querySelector(
       select.templateOf.bookTemplate).innerHTML)
   };
+  const favouriteBooks = [];
+  const bookList = document.querySelector(select.bookList);
   function render() {
     const thisBook = this;
     for(const book of dataSource.books){
       const generateHTML = templates.bookTemplate(book);
       thisBook.element = utils.createDOMFromHTML(generateHTML);
-      const container = document.querySelector(select.bookList);
-      container.appendChild(thisBook.element);
+      bookList.appendChild(thisBook.element);
+    }
+  }
+  function initActions(){
+    const bookImages= bookList.querySelectorAll(select.bookImages);
+    console.log(bookImages);
+    for(const bookImage of bookImages){
+      bookImage.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        bookImage.classList.add('favorite');
+        const dataId = bookImage.getAttribute('data-id');
+        console.log(dataId);
+        favouriteBooks.push(dataId);
+        console.log(favouriteBooks);
+      });
     }
   }
  
   render();
+  initActions();
 }
